7c517d6b132d16652e66619e6be43383a680a348
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2022-05-27 23:22:28 +0430

    bugs fixed

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ while True:
 
     solver = NewtonMethod(function_symbol, function)
 
-    print('Enter the initial point:')
+    print('Enter the initial point for root finding:')
     initial_point = float(input())
-    print('Entered function: \"f = ' + function + '\"' + ' in initial point: ' + str(initial_point))
 
     print("Starting newton's method:")
     solver.newtons_method(initial_point)
+
+    print('Enter the initial point for optimization:')
+    initial_point = float(input())
+
     print("Starting newton's optimization method:")
     solver.newtons_optimization_method(initial_point)
     print()
